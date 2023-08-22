21db50f1e2fa1a12c278ceeed72c40ceb21c75cf
김원식/PD프로젝트 <ones1k95@gmail.com>
2023-08-22 15:23:09 +0900

    [프로그래머스] 삼각 달팽이

---
+++ src/test/groovy/io/github/ones1kk/programers/level2/Lesson68645Test.groovy
@@ -0,0 +1,20 @@
+package io.github.ones1kk.programers.level2
+
+import spock.lang.Specification
+
+class Lesson68645Test extends Specification {
+
+    def "삼각 달팽이"() {
+        when:
+        def solution = Lesson68645.solution(n as int)
+
+        then:
+        solution == result as int[]
+
+        where:
+        n | result
+        4 | [1, 2, 9, 3, 10, 8, 4, 5, 6, 7]
+        5 | [1, 2, 12, 3, 13, 11, 4, 14, 15, 10, 5, 6, 7, 8, 9]
+        6 | [1, 2, 15, 3, 16, 14, 4, 17, 21, 13, 5, 18, 19, 20, 12, 6, 7, 8, 9, 10, 11]
+    }
+}

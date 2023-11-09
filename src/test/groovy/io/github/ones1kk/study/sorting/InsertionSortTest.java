e79947255b38a5e30b2af2b80c2c2d81b7bd21b1
ones1kk <ones1k95@gmail.com>
2023-11-09 22:00:50 +0900

    [정렬] 삽입 정렬 오타 수정

--- src/test/groovy/io/github/ones1kk/study/sorting/InsertionSortTest.java
+++ src/test/groovy/io/github/ones1kk/study/sorting/InsertionSortTest.java
@@ -1,13 +1,12 @@
 package io.github.ones1kk.study.sorting;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class InsertionSortTest {
 
@@ -16,7 +15,7 @@ class InsertionSortTest {
             "3,7,5,2,1"
             , "102,1,4,4214,123,3,6"
     })
-    @DisplayName("버블 정렬")
+    @DisplayName("삽입 정렬")
     void testInsertionSort(String value) {
         // given
         int[] arr = Arrays.stream(value.split(","))

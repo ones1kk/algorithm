48382ec9efccf432d67bef82e413b4cd58c8a46a
ones1kk <ones1k95@gmail.com>
2023-12-04 22:01:45 +0900

    [백준] day4

---
+++ src/main/java/io/github/ones1kk/day/day4/Problem10171.java
@@ -0,0 +1,11 @@
+package io.github.ones1kk.day.day4;
+
+public class Problem10171 {
+
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
---
+++ src/main/java/io/github/ones1kk/day/day4/Problem10172.java
@@ -0,0 +1,13 @@
+package io.github.ones1kk.day.day4;
+
+public class Problem10172 {
+
+    public static void main(String[] args) {
+
+        System.out.println("|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|");
+    }
+}

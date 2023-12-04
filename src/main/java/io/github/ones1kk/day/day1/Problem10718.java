eae78ef30455c142c7582143ebd58de8f1be649e
ones1kk <ones1k95@gmail.com>
2023-12-04 20:36:05 +0900

    [백준] day1

---
+++ src/main/java/io/github/ones1kk/day/day1/Problem10718.java
@@ -0,0 +1,8 @@
+package io.github.ones1kk.day.day1;
+
+public class Problem10718 {
+
+    public static void main(String[] args) {
+        System.out.print("강한친구 대한육군\n강한친구 대한육군");
+    }
+}
---
+++ src/main/java/io/github/ones1kk/day/day1/Problem2557.java
@@ -0,0 +1,8 @@
+package io.github.ones1kk.day.day1;
+
+public class Problem2557 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}

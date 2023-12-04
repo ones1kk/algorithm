02d3738da556aa9d9900b8e15d7c6e5ef78b81dd
ones1kk <ones1k95@gmail.com>
2023-12-04 20:46:04 +0900

    [백준] day2

---
+++ src/main/java/io/github/ones1kk/day/day2/Problem10869.java
@@ -0,0 +1,18 @@
+package io.github.ones1kk.day.day2;
+
+import java.util.Scanner;
+
+public class Problem10869 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A + B);
+        System.out.println(A - B);
+        System.out.println(A * B);
+        System.out.println(A / B);
+        System.out.println(A % B);
+    }
+}
---
+++ src/main/java/io/github/ones1kk/day/day2/Problem10998.java
@@ -0,0 +1,14 @@
+package io.github.ones1kk.day.day2;
+
+import java.util.Scanner;
+
+public class Problem10998 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A * B);
+    }
+}

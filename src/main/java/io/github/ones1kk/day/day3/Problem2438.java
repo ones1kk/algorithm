9dc1f6b5baa3cd9305a88e6901af93033a1b8ecd
ones1kk <ones1k95@gmail.com>
2023-12-04 20:57:15 +0900

    [백준] day3

---
+++ src/main/java/io/github/ones1kk/day/day3/Problem2438.java
@@ -0,0 +1,18 @@
+package io.github.ones1kk.day.day3;
+
+import java.util.Scanner;
+
+public class Problem2438 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/main/java/io/github/ones1kk/day/day3/Problem2739.java
@@ -0,0 +1,16 @@
+package io.github.ones1kk.day.day3;
+
+import java.util.Scanner;
+
+public class Problem2739 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        for (int i = 1; i < 10; i++) {
+            System.out.println(a + " * " + i + " = " + (a * i));
+        }
+
+    }
+}

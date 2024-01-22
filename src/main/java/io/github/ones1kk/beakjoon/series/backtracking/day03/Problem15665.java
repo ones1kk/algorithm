1e6981d8afbb9270d9b1680062253ae3f73de91f
김원식/PD프로젝트 <ones1k95@gmail.com>
2024-01-22 14:57:51 +0900

    [백준] 백트래킹 day03

---
+++ src/main/java/io/github/ones1kk/beakjoon/series/backtracking/day03/Problem15665.java
@@ -0,0 +1,58 @@
+package io.github.ones1kk.beakjoon.series.backtracking.day03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Problem15665 {
+
+    static int n, m;
+    static int[] answer;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        answer = new int[m];
+        arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        dfs(0);
+
+        System.out.println(sb);
+
+        br.close();
+    }
+
+    private static void dfs(int depth) {
+        if (depth == m) {
+            for (int i : answer) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int before = 0;
+        for (int i = 0; i < n; i++) {
+            if (before != arr[i]) {
+                before = arr[i];
+                answer[depth] = arr[i];
+                dfs(depth + 1);
+            }
+        }
+    }
+
+}

e268640b3fa6b043716dba5f95bef2fef2814a4c
ones1kk <ones1k95@gmail.com>
2024-06-03 23:23:13 +0900

    do it day01

---
+++ src/test/groovy/io/github/ones1kk/study/book/do_it/day01/Class002Test.groovy
@@ -0,0 +1,21 @@
+package io.github.ones1kk.study.book.do_it.day01
+
+import spock.lang.Specification
+
+class Class002Test extends Specification {
+
+    def "문제002"() {
+        when:
+        def solution = Class002.resolve(scores as int[])
+
+        then:
+        solution == result as double
+
+        where:
+        scores             || result
+        [40, 80, 60]       || 75.0
+        [3, 10]            || 65.0
+        [1, 100, 100, 100] || 75.25
+        [1, 2, 4, 8, 16]   || 38.75
+    }
+}

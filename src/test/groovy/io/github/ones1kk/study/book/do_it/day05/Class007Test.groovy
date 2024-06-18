4cd328282847490452b66415fb4b6856878f26e5
ones1kk <ones1k95@gmail.com>
2024-06-18 23:21:00 +0900

    do it day05

---
+++ src/test/groovy/io/github/ones1kk/study/book/do_it/day05/Class007Test.groovy
@@ -0,0 +1,20 @@
+package io.github.ones1kk.study.book.do_it.day05
+
+
+import spock.lang.Specification
+
+class Class007Test extends Specification {
+
+    def "문제007"() {
+        when:
+        def solution = Class007.resolve(m as int, arr as int[])
+
+        then:
+        solution == result as int
+
+        where:
+        m | arr                || result
+        9 | [2, 7, 4, 1, 5, 3] || 2
+
+    }
+}
